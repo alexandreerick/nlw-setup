57007603e0160f823e526443996545923851f6f0
Erick Alexandre <erickalexandrex@gmail.com>
2023-01-17 22:27:58 -0300

    native-wind added and home screen started

---
+++ mobile/src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <View className="bg-background flex-1 px-8 pt-16">
+      <Header />
+    </View>
+  )
+}
+
+export default Home;
